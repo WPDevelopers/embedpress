4de9a65208743c229b6c416d1c1a4eff07fdf803
Denison Martins <denisongmartins@gmail.com>
2016-04-27 12:23:51 -0300

    Fix folder name

--- embedpress/includes.php
+++ embedpress/includes.php
@@ -3,10 +3,10 @@ defined('ABSPATH') or die("No direct script access allowed.");
 
 require_once plugin_dir_path(__FILE__) .'defines.php';
 require_once EMBEDPRESS_LIBRARIES_PATH .'/autoload.php';
-require_once EMBEDPRESS_PATH .'/src/Loader.php';
-require_once EMBEDPRESS_PATH .'/src/Disabler.php';
+require_once EMBEDPRESS_PATH .'/embedpress/Loader.php';
+require_once EMBEDPRESS_PATH .'/embedpress/Disabler.php';
 require_once EMBEDPRESS_PATH .'/ends/Handler.php';
 require_once EMBEDPRESS_PATH .'/ends/admin/Admin.php';
-require_once EMBEDPRESS_PATH .'/src/Shortcode.php';
-require_once EMBEDPRESS_PATH .'/src/Plugin.php';
+require_once EMBEDPRESS_PATH .'/embedpress/Shortcode.php';
+require_once EMBEDPRESS_PATH .'/embedpress/Plugin.php';
 ?>
