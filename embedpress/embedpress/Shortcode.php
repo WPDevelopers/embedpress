dbd4cebfec794eb9a8650ff4cae2ae8c6cade506
Denison Martins <denisongmartins@gmail.com>
2016-05-19 16:44:33 -0300

    Responsive embeds now have the correct css class.

--- embedpress/embedpress/Shortcode.php
+++ embedpress/embedpress/Shortcode.php
@@ -235,7 +235,7 @@ class Shortcode
         }
 
         if ($embedShouldBeResponsive && !$embedShouldHaveCustomDimensions) {
-            $attributes['class'][] = 'ose-{provider_alias}';
+            $attributes['class'][] = 'ose-{provider_alias} responsive';
         } else {
             $attributes['responsive'] = "false";
         }
