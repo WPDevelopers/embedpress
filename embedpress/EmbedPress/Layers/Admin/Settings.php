2bae975e1a71f4d572b1841343c8c2a64f5c52aa
Denison Martins <denisongmartins@gmail.com>
2016-05-30 18:08:29 -0300

    Docs the Settings class

--- embedpress/EmbedPress/Layers/Admin/Settings.php
+++ embedpress/EmbedPress/Layers/Admin/Settings.php
@@ -5,12 +5,71 @@ use \EmbedPress\Plugin;
 
 (defined('ABSPATH') && defined('EMBEDPRESS_IS_LOADED')) or die("No direct script access allowed.");
 
+/**
+ * Entity that handles the plugin's settings page.
+ *
+ * @package     EmbedPress
+ * @subpackage  EmbedPress/Layers/Admin
+ * @author      OSTraining <support@ostraining.com>
+ * @copyright   2016 Alledia.com, All rights reserved
+ * @license     GPLv2 or later
+ * @since       0.1
+ */
 class Settings
 {
+    /**
+     * This class namespace.
+     *
+     * @since   0.1
+     * @access  private
+     * @static
+     *
+     * @var     string    $namespace
+     */
     private static $namespace = '\EmbedPress\Layers\Admin\Settings';
+
+    /**
+     * The plugin's unique identifier.
+     *
+     * @since   0.1
+     * @access  private
+     * @static
+     *
+     * @var     string    $identifier
+     */
     private static $identifier = "plg_embedpress";
+
+    /**
+     * Unique identifier to the plugin's admin settings section.
+     *
+     * @since   0.1
+     * @access  private
+     * @static
+     *
+     * @var     string    $sectionAdminIdentifier
+     */
     private static $sectionAdminIdentifier = "embedpress_options_admin";
+
+    /**
+     * Unique identifier to the plugin's general settings section.
+     *
+     * @since   0.1
+     * @access  private
+     * @static
+     *
+     * @var     string    $sectionGroupIdentifier    The name of the plugin.
+     */
     private static $sectionGroupIdentifier = "embedpress_options";
+
+    /**
+     * Map to all settings.
+     *
+     * @since   0.1
+     * @access  private
+     * @static
+     *
+     * @var     string    $fieldMap
+     */
     private static $fieldMap = array(
         'enablePluginInAdmin' => array(
             'label'   => "Enable EmbedPress in the admin area",
@@ -22,17 +81,39 @@ class Settings
         )
     );
 
+    /**
+     * Class constructor. This prevents the class being directly instantiated.
+     *
+     * @since   0.1
+     */
     public function __construct()
     {}
 
+    /**
+     * This prevents the class being cloned.
+     *
+     * @since   0.1
+     */
     public function __clone()
     {}
 
+    /**
+     * Method that adds an sub-item for EmbedPress to the WordPress Settings menu.
+     *
+     * @since   0.1
+     * @static
+     */
     public static function registerMenuItem()
     {
         add_options_page('EmbedPress Settings', 'EmbedPress', 'manage_options', 'embedpress', array(self::$namespace, 'renderForm'));
     }
 
+    /**
+     * Method that configures the EmbedPress settings page.
+     *
+     * @since   0.1
+     * @static
+     */
     public static function registerActions()
     {
         register_setting(self::$sectionGroupIdentifier, self::$sectionGroupIdentifier, array(self::$namespace, "validateForm"));
@@ -44,6 +125,12 @@ class Settings
         }
     }
 
+    /**
+     * Method that render the settings's form.
+     *
+     * @since   0.1
+     * @static
+     */
     public static function renderForm()
     {
         ?>
@@ -59,6 +146,16 @@ class Settings
         <?php
     }
 
+    /**
+     * Method that validates the form data.
+     *
+     * @since   0.1
+     * @static
+     *
+     * @param   mixed   $freshData  Data received from the form.
+     *
+     * @return  array
+     */
     public static function validateForm($freshData)
     {
         $data = array(
@@ -69,9 +166,25 @@ class Settings
         return $data;
     }
 
+    /**
+     * Method that prints help info for the form.
+     *
+     * @since   0.1
+     * @static
+     *
+     * @return  string
+     */
     public static function renderHelpText()
-    {}
+    {
+        return "";
+    }
 
+    /**
+     * Method that renders the displayPreviewBox input.
+     *
+     * @since   0.1
+     * @static
+     */
     public static function renderField_displayPreviewBox()
     {
         $fieldName = "displayPreviewBox";
@@ -91,6 +204,12 @@ class Settings
         echo '<label><input type="radio" id="'. $fieldName .'_1" name="'. self::$sectionGroupIdentifier .'['. $fieldName .']" value="1" '. ($options[$fieldName] ? "checked" : "") .' /> Yes</label>';
     }
 
+    /**
+     * Method that renders the enablePluginInAdmin input.
+     *
+     * @since   0.1
+     * @static
+     */
     public static function renderField_enablePluginInAdmin()
     {
         $fieldName = "enablePluginInAdmin";
