6f548bb32b2781e9fbf219cce9faea6eccd1a9f6
Denison Martins <denisongmartins@gmail.com>
2016-05-31 16:25:50 -0300

    Doc the providers.php file

--- embedpress/providers.php
+++ embedpress/providers.php
@@ -1,6 +1,19 @@
 <?php
 (defined('ABSPATH') && defined('EMBEDPRESS_IS_LOADED')) or die("No direct script access allowed.");
 
+/**
+ * Declare an associative array that is responsible for mapping additional service providers to its urls.
+ * The key must match the class placed in ./EmbedPress/Providers/ folder, and the values must be a string or
+ * another array listing all url patterns in which the key (a.k.a. the service provider you're adding)
+ * should be triggered.
+ *
+ * @package     EmbedPress
+ * @author      PressShack.com <help@pressshack.com>
+ * @copyright   Copyright (C) 2016 Open Source Training, LLC. All rights reserved.
+ * @license     GPLv2 or later
+ * @since       0.1
+ */
+
 $additionalServiceProviders = array(
     'GoogleMaps' => ["google.com", "google.com.*", "maps.google.com"],
     'GoogleDocs' => ["docs.google.com"]
