6817f4111a00639a40440774c3757419cd4fc5f3
Denison Martins <denisongmartins@gmail.com>
2017-01-26 11:36:30 -0200

    Update year in copyright

--- src/EmbedPress/Disabler.php
+++ src/EmbedPress/Disabler.php
@@ -10,7 +10,7 @@ use \EmbedPress\Shortcode;
  *
  * @package     EmbedPress
  * @author      PressShack <help@pressshack.com>
- * @copyright   Copyright (C) 2016 Open Source Training, LLC. All rights reserved.
+ * @copyright   Copyright (C) 2017 Open Source Training, LLC. All rights reserved.
  * @license     GPLv2 or later
  * @since       1.0.0
  */
