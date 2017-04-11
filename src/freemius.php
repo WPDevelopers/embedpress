3893a5f77e32fd9490221355d4ccf6d837b346a2
Anderson Grudtner Martins <anderson@ostraining.com>
2017-04-11 17:09:50 -0300

    Fixes the since comment

--- src/freemius.php
+++ src/freemius.php
@@ -6,7 +6,7 @@ defined('ABSPATH') or die("No direct script access allowed.");
  * @author      PressShack <help@pressshack.com>
  * @copyright   Copyright (C) 2017 Open Source Training, LLC. All rights reserved.
  * @license     GPLv2 or later
- * @since       1.7.0
+ * @since       1.7.1
  */
 
 // Create a helper function for easy SDK access.
