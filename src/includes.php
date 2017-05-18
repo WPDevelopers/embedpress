181e24df6630c1cd7151ad175e9da0268f746142
Anderson Grudtner Martins <anderson@ostraining.com>
2017-05-18 12:13:45 -0300

    Update version

--- src/includes.php
+++ src/includes.php
@@ -22,7 +22,7 @@ if (!defined('EMBEDPRESS_PLG_NAME')) {
 }
 
 if (!defined('EMBEDPRESS_PLG_VERSION')) {
-    define('EMBEDPRESS_PLG_VERSION', "1.7.3");
+    define('EMBEDPRESS_PLG_VERSION', "1.7.4");
 }
 
 if (!defined('EMBEDPRESS_PATH_BASE')) {
