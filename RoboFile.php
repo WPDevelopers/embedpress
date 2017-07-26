15a7fb46bfcdc996a1c669829a19c909173b622e
Anderson Grudtner Martins <anderson@ostraining.com>
2017-07-26 20:44:15 -0300

    Updated the builder file

---
+++ RoboFile.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'vendor/autoload.php';
+
+/**
+ * This is project's console commands configuration for Robo task runner.
+ *
+ * @see http://robo.li/
+ */
+class RoboFile extends PressShack\Builder\Robo_Task {
+
+    public function __construct() {
+        $this->plugin_name      = 'embedpress';
+        $this->version_constant = 'EMBEDPRESS_PLG_VERSION';
+    }
+}
