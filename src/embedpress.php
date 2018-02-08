ca6aee00b8c38778378e4b5509ba1becdbd820af
Anderson Grudtner Martins <anderson@ostraining.com>
2018-02-08 12:41:58 -0200

    Update version to 2.1.0

--- src/embedpress.php
+++ src/embedpress.php
@@ -5,7 +5,7 @@
  * Description: WordPress supports around 35 embed sources, but PublishPress Embeds adds over 40 more, including Facebook, Google Maps, Google Docs, UStream! Just use the URL!
  * Author:      EmbedPress
  * Author URI:  http://embedpress.com
- * Version:     2.0.3
+ * Version:     2.1.0
  * Text Domain: embedpress
  * Domain Path: /languages
  *
