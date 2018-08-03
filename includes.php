285c740b244e52aa0c55300f6a6a51f72f6c0a8e
Anderson Grudtner Martins <anderson@alledia.com>
2018-08-03 16:55:26 -0300

    Update version number to 2.1.6

--- includes.php
+++ includes.php
@@ -21,7 +21,7 @@ if (!defined('EMBEDPRESS_PLG_NAME')) {
 }
 
 if (!defined('EMBEDPRESS_PLG_VERSION')) {
-    define('EMBEDPRESS_PLG_VERSION', "2.1.5");
+    define('EMBEDPRESS_PLG_VERSION', "2.1.6");
 }
 
 if (!defined('EMBEDPRESS_ROOT')) {
