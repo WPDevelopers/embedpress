e28a47b98f6c595ea15c84405333f14440597633
Manzur Ahammed <manzur.ahammed236@gmail.com>
2020-06-07 01:10:22 +0600

    add upload option

--- Gutenberg/src/blocks.js
+++ Gutenberg/src/blocks.js
@@ -18,6 +18,7 @@ import './google-maps/index.js';
 import './twitch/index.js';
 import './wistia/index.js';
 import './youtube/index.js';
+import './document/index.js';
 
 ( function() {
 		let a = <svg width="33" height="20" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
---
+++ Gutenberg/src/document/edit.js
@@ -0,0 +1,161 @@
+/**
+ * Internal dependencies
+ */
+import EmbedControls from '../common/embed-controls';
+import EmbedLoading from '../common/embed-loading';
+import EmbedPlaceholder from '../common/embed-placeholder';
+import Iframe from '../common/Iframe';
+
+/**
+ * WordPress dependencies
+ */
+import classnames from 'classnames';
+
+const {__} = wp.i18n;
+const {getBlobByURL, isBlobURL, revokeBlobURL} = wp.blob;
+const {BlockControls, BlockIcon, MediaPlaceholder, MediaReplaceFlow} = wp.editor;
+const {Component, Fragment} = wp.element;
+const {BaseControl, Button, Disabled, PanelBody, withNotices,} = wp.components;
+import {googleSlidesIcon} from '../common/icons'
+
+const ALLOWED_MEDIA_TYPES = [
+	'application/pdf',
+	'application/msword',
+	'application/vnd.ms-powerpoint',
+	'application/vnd.ms-excel',
+	'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+	'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+	'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+];
+
+class DocumentEdit extends Component {
+	constructor() {
+		super(...arguments);
+		this.onSelectFile = this.onSelectFile.bind(this);
+		this.confirmCopyURL = this.confirmCopyURL.bind(this);
+		this.resetCopyConfirmation = this.resetCopyConfirmation.bind(this);
+		this.changeLinkDestinationOption = this.changeLinkDestinationOption.bind(
+			this
+		);
+		this.changeOpenInNewWindow = this.changeOpenInNewWindow.bind(this);
+		this.onUploadError = this.onUploadError.bind(this);
+		this.state = {
+			hasError: false,
+			showCopyConfirmation: false,
+		};
+	}
+
+
+	componentDidMount() {
+		const {
+			attributes,
+			mediaUpload,
+			noticeOperations
+		} = this.props;
+		const {href} = attributes;
+
+		// Upload a file drag-and-dropped into the editor
+		if (isBlobURL(href)) {
+			const file = getBlobByURL(href);
+
+			mediaUpload({
+				filesList: [file],
+				onFileChange: ([media]) => this.onSelectFile(media),
+				onError: (message) => {
+					this.setState({hasError: true});
+					noticeOperations.createErrorNotice(message);
+				},
+			});
+
+			revokeBlobURL(href);
+		}
+	}
+
+	componentDidUpdate(prevProps) {
+		// Reset copy confirmation state when block is deselected
+		if (prevProps.isSelected && !this.props.isSelected) {
+			this.setState({showCopyConfirmation: false});
+		}
+	}
+
+	onSelectFile(media) {
+		if (media && media.url) {
+			console.log(media);
+			this.setState({hasError: false});
+			this.props.setAttributes({
+				href: media.url,
+				fileName: media.title,
+				textLinkHref: media.url,
+				id: media.id,
+			});
+		}
+	}
+
+	onUploadError(message) {
+		const {noticeOperations} = this.props;
+		noticeOperations.removeAllNotices();
+		noticeOperations.createErrorNotice(message);
+	}
+
+	confirmCopyURL() {
+		this.setState({showCopyConfirmation: true});
+	}
+
+	resetCopyConfirmation() {
+		this.setState({showCopyConfirmation: false});
+	}
+
+	changeLinkDestinationOption(newHref) {
+		// Choose Media File or Attachment Page (when file is in Media Library)
+		this.props.setAttributes({textLinkHref: newHref});
+	}
+
+	changeOpenInNewWindow(newValue) {
+		this.props.setAttributes({
+			textLinkTarget: newValue ? '_blank' : false,
+		});
+	}
+
+
+	render() {
+		console.log(this.state);
+		const {className, isSelected, attributes, setAttributes, noticeUI, media} = this.props;
+		const {
+			id,
+			fileName,
+			href,
+			textLinkHref,
+			textLinkTarget
+		} = attributes;
+		const {hasError, showCopyConfirmation} = this.state;
+		const attachmentPage = media && media.link;
+
+		if (!href || hasError) {
+
+			return (
+				<MediaPlaceholder
+					icon={<BlockIcon icon={googleSlidesIcon}/>}
+					labels={{
+						title: __('Document'),
+						instructions: __(
+							'Upload a file or pick one from your media library for embed'
+						),
+					}}
+					onSelect={this.onSelectFile}
+					notices={noticeUI}
+					allowedTypes={ALLOWED_MEDIA_TYPES}
+					onError={this.onUploadError}
+				/>
+			);
+		} else {
+			return (
+				<Fragment>
+					<div>Upload File</div>
+				</Fragment>
+			);
+		}
+
+	}
+
+};
+export default DocumentEdit;
