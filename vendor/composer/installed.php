ccb6b6c60d63ccce959d77404ca6d254d159f3bd
Kamal Ahmed <kamalacca@gmail.com>
2021-02-25 16:04:20 +0600

    added embera to main git repo and reduced vendor folder size

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ce096bb496cd88d9c2ebadc239310813a7be244d',
+    'name' => 'embedpress/embedpress',
+  ),
+  'versions' => 
+  array (
+    'embedpress/embedpress' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ce096bb496cd88d9c2ebadc239310813a7be244d',
+    ),
+    'wpdevelopers/embera' => 
+    array (
+      'pretty_version' => '2.0.17',
+      'version' => '2.0.17.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5f521fbe77fdfc992dc399ee39e5fdb338ea1f04',
+    ),
+  ),
+);
