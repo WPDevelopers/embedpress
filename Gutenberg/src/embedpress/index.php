af44a711e802d804a8b55ab0df269b6778b903c8
Kamal Ahmed <kamalacca@gmail.com>
2021-03-03 18:31:18 +0600

    make gutenberg block responsive on frontend

--- Gutenberg/src/embedpress/index.php
+++ Gutenberg/src/embedpress/index.php
@@ -10,9 +10,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 function embedpress_render_block($attributes){
 	if ( !empty( $attributes['embedHTML']) ) {
 		$embed         = apply_filters( 'embedpress_gutenberg_embed', $attributes['embedHTML'], $attributes );
+		$ats ='';
+		if(isset( $attributes['height'])){
+			$ats .='height:'. esc_attr($attributes['height']) .'px;';
+			$ats .='max-height:100%;';
+		}
+		if(isset( $attributes['width'])){
+			$ats .='width:'. esc_attr($attributes['width']) .'px;';
+			$ats .='max-width:100%;';
+		}
 		ob_start();
 		?>
-		<div class="embedpress-gutenberg-wrapper">
+		<div class="embedpress-gutenberg-wrapper" style="<?php echo esc_attr($ats);?>">
 			<?php echo $embed; ?>
 		</div>
 		<?php
