117dd8e18a2c374570d683076214bbaf6f3fbeb6
Kamal Ahmed <kamalacca@gmail.com>
2021-03-21 13:33:10 +0600

    updated settings page to remove general settings

--- EmbedPress/Ends/Back/Settings.php
+++ EmbedPress/Ends/Back/Settings.php
@@ -2,8 +2,6 @@
 
 namespace EmbedPress\Ends\Back;
 
-use EmbedPress\Compatibility;
-
 (defined( 'ABSPATH' ) && defined( 'EMBEDPRESS_IS_LOADED' )) or die( "No direct script access allowed." );
 
 /**
@@ -27,8 +25,6 @@ class Settings {
      * @var     string $namespace
      */
     private static $namespace = '\\EmbedPress\\Ends\\Back\\Settings';
-
-    private static $has_general_settings_fields = null;
 
     /**
      * The plugin's unique identifier.
@@ -98,53 +94,12 @@ class Settings {
      */
     public static function registerActions() {
         $activeTab = isset( $_GET['tab'] ) ? strtolower( $_GET['tab'] ) : "";
-        if ( $activeTab !== "embedpress" ) {
-            $action = "embedpress:{$activeTab}:settings:register";
-        } else {
-            $activeTab = "";
-        }
-
-        if ( !empty( $activeTab ) && has_action( $action ) ) {
+	    $action = ( $activeTab !== "embedpress" ) ? "embedpress:{$activeTab}:settings:register" : '';
+	    if ( !empty( $activeTab ) && has_action( $action ) ) {
             do_action( $action, [
                 'id'   => self::$sectionAdminIdentifier,
                 'slug' => self::$identifier,
             ] );
-        } else {
-            register_setting( self::$sectionGroupIdentifier, self::$sectionGroupIdentifier,
-                [ self::$namespace, "validateForm" ] );
-
-            add_settings_section( self::$sectionAdminIdentifier, '', null, self::$identifier );
-
-            $fieldMap = [];
-            if ( !Compatibility::isWordPress5() || Compatibility::isClassicalEditorActive() ) {
-                $fieldMap = [
-                    'enablePluginInAdmin'     => [
-                        'label'   => __("Load previews in the admin editor", "embedpress"),
-                        'section' => "admin",
-                    ],
-                    'enablePluginInFront'     => [
-                        'label'   => __("Load previews in the frontend editor", "embedpress"),
-                        'section' => "admin",
-                    ],
-                    'enableGlobalEmbedResize' => [
-                        'label'   => __("Enable Global Embed Dimension", "embedpress"),
-                        'section' => "admin",
-                    ],
-                    'enableEmbedResizeWidth'  => [
-                        'label'   => __("Embed Iframe Width", "embedpress"),
-                        'section' => "admin",
-                    ],
-                    'enableEmbedResizeHeight' => [
-                        'label'   => __("Embed Iframe Height", "embedpress"),
-                        'section' => "admin",
-                    ]
-                ];
-            }
-            self::$has_general_settings_fields = !empty( $fieldMap);
-            foreach ( $fieldMap as $fieldName => $field ) {
-                add_settings_field( $fieldName, $field['label'], [ self::$namespace, "renderField_{$fieldName}" ],
-                    self::$identifier, self::${"section" . ucfirst( $field['section'] ) . "Identifier"} );
-            }
         }
     }
 
@@ -171,7 +126,7 @@ class Settings {
     }
 
     /**
-     * Method that render the settings's form.
+     * Method that render the settings form.
      *
      * @since   1.0.0
      * @static
@@ -184,7 +139,8 @@ class Settings {
             EMBEDPRESS_VERSION, true );
 
         $activeTab                  = isset( $_GET['tab'] ) ? strtolower( $_GET['tab'] ) : "";
-        $is_gen_tab_active          = ($activeTab === 'embedpress' || empty( $activeTab ));
+        $is_upgrade_tab_active = ($activeTab === 'embedpress_get_pro' || empty( $activeTab )) && !defined( 'EMBEDPRESS_PRO_PLUGIN_VERSION' );
+        $is_license_tab_active = ($activeTab === 'embedpress_license' || empty( $activeTab )) && defined( 'EMBEDPRESS_PRO_PLUGIN_VERSION' );
         $settingsFieldsIdentifier   = !empty( $activeTab ) ? "embedpress:{$activeTab}" : self::$sectionGroupIdentifier;
         $settingsSectionsIdentifier = !empty( $activeTab ) ? "embedpress:{$activeTab}" : self::$identifier;
         $five_stars = `
@@ -214,13 +170,9 @@ class Settings {
             <?php settings_errors(); ?>
             <div>
                 <h2 class="nav-tab-wrapper">
-                    <a href="?page=embedpress"
-                       class="nav-tab<?php echo $is_gen_tab_active ? ' nav-tab-active' : ''; ?> ">
-	                    <?php esc_html_e( 'General settings', 'embedpress'); ?>
-                    </a>
                     <?php if ( !defined( 'EMBEDPRESS_PRO_PLUGIN_VERSION' ) ): ?>
                         <a href="?page=embedpress&tab=embedpress_get_pro"
-                           class="nav-tab<?php echo $activeTab === 'embedpress_get_pro' ? ' nav-tab-active' : ''; ?> ">
+                           class="nav-tab<?php echo $is_upgrade_tab_active ? ' nav-tab-active' : ''; ?> ">
 	                        <?php esc_html_e( 'Go Premium', 'embedpress'); ?>
                         </a>
                     <?php endif; ?>
@@ -232,26 +184,18 @@ class Settings {
                     <form action="options.php" method="POST" style="padding-bottom: 20px;">
                         <?php settings_fields( $settingsFieldsIdentifier ); ?>
                         <?php do_settings_sections( $settingsSectionsIdentifier ); ?>
-                        <?php if (
-                                $activeTab !== 'embedpress_license'
-                                && $activeTab !== 'embedpress_get_pro'
-                                && !($is_gen_tab_active && !self::$has_general_settings_fields)
-                        ) {
+                        <?php if ( !$is_license_tab_active  && !$is_upgrade_tab_active ) {
                             ?>
                                 <button type="submit" class="button button-primary embedpress-setting-save"><?php esc_html_e( 'Save Changes', 'embedpress'); ?>
                                 </button>
                         <?php } ?>
-                        <?php
-                        if($is_gen_tab_active && !self::$has_general_settings_fields){
-                            printf( "<h2 style='text-align: center'>%s</h2>", __("Welcome to EmbedPress", 'embedpress'));
-                        }
-                        ?>
+
                     </form>
                 <?php endif; ?>
-                <?php if ( $activeTab == 'embedpress_license' ) : ?>
+                <?php if ( $is_license_tab_active ) : ?>
                     <?php do_action( 'embedpress_license' ); ?>
                 <?php endif; ?>
-                <?php if ( $activeTab == 'embedpress_get_pro' && !defined( 'EMBEDPRESS_PRO_PLUGIN_VERSION' ) ) : ?>
+                <?php if ( $is_upgrade_tab_active ) : ?>
                     <div class=" embedpress-go-premium">
                         <div class="embedpress-col-half">
                             <div class="embedpress-admin-block-wrapper">
@@ -283,7 +227,7 @@ class Settings {
                        rel="noopener noreferrer">
                         <?php
                         /* translators: 1: EmbedPress Plugin Name, 2: 5 stars. */
-                        printf( __('If you like %1$s please leave us a %2$s
+                        printf( __('If you like %1$s please leave us a 5 stars %2$s
                          rating. Thank you!', 'embedpress'), '<strong>EmbedPress</strong>', $five_stars ) ?>
                     </a>
                 </p>
@@ -324,137 +268,12 @@ class Settings {
                 </nav>
                 <p>
                     <a href="//embedpress.com" target="_blank" rel="noopener noreferrer">
-                        <img width="100" src="<?php echo plugins_url( 'assets/images/embedpress.png',
+                        <img width="100" alt="embedpress logo" src="<?php echo plugins_url( 'assets/images/embedpress.png',
                             EMBEDPRESS_PLUGIN_BASENAME ); ?>">
                     </a>
                 </p>
             </footer>
         </div>
         <?php
-    }
-
-    /**
-     * Method that validates the form data.
-     *
-     * @param mixed $freshData Data received from the form.
-     *
-     * @return  array
-     * @since   1.0.0
-     * @static
-     *
-     */
-    public static function validateForm( $freshData ) {
-        $data = [
-            'enablePluginInAdmin'     => isset( $freshData['enablePluginInAdmin'] ) ? (bool)$freshData['enablePluginInAdmin'] : true,
-            'enablePluginInFront'     => isset( $freshData['enablePluginInFront'] ) ? (bool)$freshData['enablePluginInFront'] : true,
-            'enableGlobalEmbedResize' => isset( $freshData['enableGlobalEmbedResize'] ) ? (bool)$freshData['enableGlobalEmbedResize'] : false,
-            'enableEmbedResizeHeight' => isset( $freshData['enableEmbedResizeHeight'] ) ? $freshData['enableEmbedResizeHeight'] : 552,
-            'enableEmbedResizeWidth'  => isset( $freshData['enableEmbedResizeWidth'] ) ? $freshData['enableEmbedResizeWidth'] : 652,
-        ];
-
-        return $data;
-    }
-
-    /**
-     * Method that renders the enablePluginInAdmin input.
-     *
-     * @since   1.0.0
-     * @static
-     */
-    public static function renderField_enablePluginInAdmin() {
-        $fieldName = "enablePluginInAdmin";
-
-        $options = get_option( self::$sectionGroupIdentifier );
-
-        $options[$fieldName] = !isset( $options[$fieldName] ) ? true : (bool)$options[$fieldName];
-
-        echo '<label><input type="radio" id="' . $fieldName . '_0" name="' . self::$sectionGroupIdentifier . '[' . $fieldName . ']" value="0" ' . (!$options[$fieldName] ? "checked" : "") . ' /> No</label>';
-        echo "&nbsp;&nbsp;";
-        echo '<label><input type="radio" id="' . $fieldName . '_1" name="' . self::$sectionGroupIdentifier . '[' . $fieldName . ']" value="1" ' . ($options[$fieldName] ? "checked" : "") . ' /> Yes</label>';
-        echo '<p class="description">';
-	    /* Translator: 1. <strong> tag and 2. </strong> tag */
-	    printf( __('Do you want EmbedPress to run here in the admin area? Disabling this %1$swill not%2$s affect your frontend embeds.', 'embedpress'), '<strong>', '</strong>');
-        echo '</p>';
-    }
-
-    /**
-     * Method that renders the enablePluginInFront input.
-     *
-     * @since   1.6.0
-     * @static
-     */
-    public static function renderField_enablePluginInFront() {
-        $fieldName = "enablePluginInFront";
-
-        $options = get_option( self::$sectionGroupIdentifier );
-
-        $options[$fieldName] = !isset( $options[$fieldName] ) ? true : (bool)$options[$fieldName];
-
-        echo '<label><input type="radio" id="' . $fieldName . '_0" name="' . self::$sectionGroupIdentifier . '[' . $fieldName . ']" value="0" ' . (!$options[$fieldName] ? "checked" : "") . ' /> No</label>';
-        echo "&nbsp;&nbsp;";
-        echo '<label><input type="radio" id="' . $fieldName . '_1" name="' . self::$sectionGroupIdentifier . '[' . $fieldName . ']" value="1" ' . ($options[$fieldName] ? "checked" : "") . ' /> Yes</label>';
-        echo '<p class="description">';
-	    /* Translator: 1. <strong> tag and 2. </strong> tag */
-        printf( __('Do you want EmbedPress to run within editors in frontend (if there\'s any)? Disabling this %1$swill not%2$s affect embeds seem by your regular users in frontend.'),'<strong>', '</strong>');
-	    echo '</p>';
-    }
-
-    /**
-     * Method that renders the enablePluginInAdmin input.
-     *
-     * @since   2.4.1
-     * @static
-     */
-    public static function renderField_enableGlobalEmbedResize() {
-        $fieldName = "enableGlobalEmbedResize";
-
-        $options = get_option( self::$sectionGroupIdentifier );
-
-        $options[$fieldName] = !isset( $options[$fieldName] ) ? false : (bool)$options[$fieldName];
-
-        echo '<label><input class="enableglobalembedresize" type="radio" id="' . $fieldName . '_0" name="' . self::$sectionGroupIdentifier . '[' . $fieldName . ']" value="0" ' . (!$options[$fieldName] ? "checked" : "") . ' /> No</label>';
-        echo "&nbsp;&nbsp;";
-        echo '<label><input class="enableglobalembedresize" type="radio" id="' . $fieldName . '_1" name="' . self::$sectionGroupIdentifier . '[' . $fieldName . ']" value="1" ' . ($options[$fieldName] ? "checked" : "") . ' /> Yes</label>';
-        echo '<p class="description">';
-        /* Translator: 1. <strong> tag and 2. </strong> tag */
-        printf( __('Do you want use global embed dimension, Disabling this %1$s will not%2$s affect embeds.'), '<strong>', '</strong>');
-        echo '</p>';
-    }
-
-    /**
-     * Method that renders the enableEmbedResizeHeight input.
-     *
-     * @since  2.4.0
-     * @static
-     */
-    public static function renderField_enableEmbedResizeHeight() {
-        $fieldName = "enableEmbedResizeHeight";
-
-        $options = get_option( self::$sectionGroupIdentifier );
-
-        $value = !isset( $options[$fieldName] ) ? '552' : $options[$fieldName];
-
-        echo '<span class="embedpress-allow-globla-dimension"><input type="number" value="' . absint( $value ) . '" class="regular-text" name="' . self::$sectionGroupIdentifier . '[' . $fieldName . ']">';
-
-        echo '<p class="description">';
-        _e( 'Global Embed Iframe Height', 'embedpress');
-        echo '</p></span>';
-    }
-
-    /**
-     * Method that renders the enableEmbedResizeWidth input.
-     *
-     * @since  2.4.0
-     * @static
-     */
-    public static function renderField_enableEmbedResizeWidth() {
-        $fieldName = "enableEmbedResizeWidth";
-        $options   = get_option( self::$sectionGroupIdentifier );
-        $value     = !isset( $options[$fieldName] ) ? '652' : $options[$fieldName];
-
-        echo '<span class="embedpress-allow-globla-dimension"><input type="number" value="' . absint( $value ) . '" class="regular-text" name="' . self::$sectionGroupIdentifier . '[' . $fieldName . ']">';
-	    echo '<p class="description">';
-	    _e( 'Global Embed Iframe Width', 'embedpress');
-	    echo '</p></span>';
     }
 }
