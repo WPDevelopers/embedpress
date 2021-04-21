48c2143dd7747667c8672aea10cc3e90fd6bc31a
Kamal Ahmed <kamalacca@gmail.com>
2021-04-21 17:02:33 +0600

    made all text in vimeo page translatable

--- EmbedPress/Ends/Back/Settings/templates/vimeo.php
+++ EmbedPress/Ends/Back/Settings/templates/vimeo.php
@@ -8,122 +8,122 @@
         <form action="" method="post" >
 	        <?php echo  $nonce_field ; ?>
             <div class="form__group">
-                <p class="form__label">Auto Play</p>
+                <p class="form__label"><?php esc_html_e( "Auto Play", "embedpress" ); ?></p>
                 <div class="form__control__wrap">
                     <div class="input__flex">
                         <label class="input__radio">
                             <input type="radio" name="autoplay">
-                            <span>No</span>
+                            <span><?php esc_html_e( "No", "embedpress" ); ?></span>
                         </label>
                         <label class="input__radio">
                             <input type="radio" name="autoplay">
-                            <span>Yes</span>
+                            <span><?php esc_html_e( "Yes", "embedpress" ); ?></span>
                         </label>
                     </div>
-                    <p>Automatically start to play the videos when the player loads.</p>
+                    <p><?php esc_html_e( "Automatically start to play the videos when the player loads.", "embedpress" ); ?></p>
                 </div>
             </div>
             <div class="form__group">
-                <p class="form__label">Loop</p>
+                <p class="form__label"><?php esc_html_e( "Loop", "embedpress" ); ?></p>
                 <div class="form__control__wrap">
                     <div class="input__flex">
                         <label class="input__radio">
                             <input type="radio" name="loop">
-                            <span>No</span>
+                            <span><?php esc_html_e( "No", "embedpress" ); ?></span>
                         </label>
                         <label class="input__radio">
                             <input type="radio" name="loop">
-                            <span>Yes</span>
+                            <span><?php esc_html_e( "Yes", "embedpress" ); ?></span>
                         </label>
                     </div>
-                    <p>Play the video again automatically when it reaches the end.</p>
+                    <p><?php esc_html_e( "Play the video again automatically when it reaches the end.", "embedpress" ); ?></p>
                 </div>
             </div>
             <div class="form__group">
-                <p class="form__label">Autopause <span class="isPro">Pro</span></p>
+                <p class="form__label"><?php esc_html_e( "Auto pause", "embedpress" ); ?> <span class="isPro">Pro</span></p>
                 <div class="form__control__wrap">
                     <div class="input__flex">
                         <label class="input__radio">
                             <input type="radio" name="autopause" disabled>
-                            <span>No</span>
+                            <span><?php esc_html_e( "No", "embedpress" ); ?></span>
                         </label>
                         <label class="input__radio">
                             <input type="radio" name="autopause" disabled>
-                            <span>Yes</span>
+                            <span><?php esc_html_e( "Yes", "embedpress" ); ?></span>
                         </label>
                     </div>
-                    <p>Pause this video automatically when another one plays.</p>
+                    <p><?php esc_html_e( "Pause this video automatically when another one plays.", "embedpress" ); ?></p>
                 </div>
             </div>
             <div class="form__group">
-                <p class="form__label">DNT</p>
+                <p class="form__label"><?php esc_html_e( "DNT", "embedpress" ); ?></p>
                 <div class="form__control__wrap">
                     <div class="input__flex">
                         <label class="input__radio">
                             <input type="radio" name="dnt">
-                            <span>No</span>
+                            <span><?php esc_html_e( "No", "embedpress" ); ?></span>
                         </label>
                         <label class="input__radio">
                             <input type="radio" name="dnt">
-                            <span>Yes</span>
+                            <span><?php esc_html_e( "Yes", "embedpress" ); ?></span>
                         </label>
                     </div>
-                    <p>Setting this parameter to "yes" will block the player from tracking any session data, including all cookies</p>
+                    <p><?php esc_html_e( 'Setting this parameter to "yes" will block the player from tracking any session data, including all cookies', "embedpress" ); ?></p>
                 </div>
             </div>
             <div class="form__group">
-                <p class="form__label">Color</p>
+                <p class="form__label"><?php esc_html_e( "Color", "embedpress" ); ?></p>
                 <div class="form__control__wrap">
-                    <a href="#" class="button radius-10">Select Color</a>
-                    <p>Specify the color of the video controls.</p>
+                    <a href="#" class="button radius-10"><?php esc_html_e( "Select Color", "embedpress" ); ?></a>
+                    <p><?php esc_html_e( "Specify the color of the video controls.", "embedpress" ); ?></p>
                 </div>
             </div>
             <div class="form__group">
-                <p class="form__label">Display Title</p>
+                <p class="form__label"><?php esc_html_e( "Display Title", "embedpress" ); ?></p>
                 <div class="form__control__wrap">
                     <div class="input__flex">
                         <label class="input__radio">
                             <input type="radio" name="displaytitle">
-                            <span>No</span>
+                            <span><?php esc_html_e( "No", "embedpress" ); ?></span>
                         </label>
                         <label class="input__radio">
                             <input type="radio" name="displaytitle">
-                            <span>Yes</span>
+                            <span><?php esc_html_e( "Yes", "embedpress" ); ?></span>
                         </label>
                     </div>
-                    <p>Indicates whether the title is displayed.</p>
+                    <p><?php esc_html_e( "Indicates whether the title is displayed.", "embedpress" ); ?></p>
                 </div>
             </div>
             <div class="form__group">
-                <p class="form__label">Display Author</p>
+                <p class="form__label"><?php esc_html_e( "Display Author", "embedpress" ); ?></p>
                 <div class="form__control__wrap">
                     <div class="input__flex">
                         <label class="input__radio">
                             <input type="radio" name="displayauthor">
-                            <span>No</span>
+                            <span><?php esc_html_e( "No", "embedpress" ); ?></span>
                         </label>
                         <label class="input__radio">
                             <input type="radio" name="displayauthor">
-                            <span>Yes</span>
+                            <span><?php esc_html_e( "Yes", "embedpress" ); ?></span>
                         </label>
                     </div>
-                    <p>Indicates whether the author is displayed.</p>
+                    <p><?php esc_html_e( "Indicates whether the author is displayed.", "embedpress" ); ?></p>
                 </div>
             </div>
             <div class="form__group">
-                <p class="form__label">Display Avatar <span class="isPro">Pro</span></p>
+                <p class="form__label"><?php esc_html_e( "Display Avatar", "embedpress" ); ?> <span class="isPro">Pro</span></p>
                 <div class="form__control__wrap">
                     <div class="input__flex">
                         <label class="input__radio">
                             <input type="radio" name="displayavatar" disabled>
-                            <span>No</span>
+                            <span><?php esc_html_e( "No", "embedpress" ); ?></span>
                         </label>
                         <label class="input__radio">
                             <input type="radio" name="displayavatar" disabled>
-                            <span>Yes</span>
+                            <span><?php esc_html_e( "Yes", "embedpress" ); ?></span>
                         </label>
                     </div>
-                    <p>Indicates whether the avatar is displayed.</p>
+                    <p><?php esc_html_e( "Indicates whether the avatar is displayed.", "embedpress" ); ?></p>
                 </div>
             </div>
             <button class="button button__themeColor radius-10" name="submit" value="vimeo"><?php esc_html_e( 'Save Changes', 'embedpress'); ?></button>
