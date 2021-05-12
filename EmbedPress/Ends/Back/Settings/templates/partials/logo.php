84fe2041f1cc68c60ac55730f6494ef9864abc3c
Rojab Khan <irojabkhan@gmail.com>
2021-05-12 12:29:30 +0600

    version number added

--- EmbedPress/Ends/Back/Settings/templates/partials/logo.php
+++ EmbedPress/Ends/Back/Settings/templates/partials/logo.php
@@ -3,12 +3,12 @@
  * Footer of the settings page
  * */
 ?>
-<header class="embedpress-header" style="display: flex; justify-content: space-between; align-items: center">
+<header class="embedpress-header">
     <a href="#" class="site__logo"><img src="<?php echo EMBEDPRESS_SETTINGS_ASSETS_URL; ?>img/logo.svg" alt=""></a>
     <ul class="embedpress-version-wrapper">
-        <li>EmbedPress: V<?php echo EMBEDPRESS_VERSION; ?></li>
+        <li>Core Version: V<?php echo EMBEDPRESS_VERSION; ?></li>
 	    <?php if ( defined( 'EMBEDPRESS_PRO_VERSION') ) { ?>
-            <li>EmbedPress Pro: V<?php echo EMBEDPRESS_PRO_VERSION; ?></li>
+            <li>Pro Version: V<?php echo EMBEDPRESS_PRO_VERSION; ?></li>
 	    <?php }?>
     </ul>
 </header>
