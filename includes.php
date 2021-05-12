6bc6764e1c544ade9d83415d2c32d4d49564330b
Kamal Ahmed <kamalacca@gmail.com>
2021-05-12 15:26:26 +0600

    bumped version to v3.0.0 for both free and pro

--- includes.php
+++ includes.php
@@ -22,7 +22,7 @@ if ( ! defined('EMBEDPRESS_PLG_NAME')) {
 }
 
 if ( ! defined('EMBEDPRESS_VERSION')) {
-    define('EMBEDPRESS_VERSION', "2.7.7");
+    define('EMBEDPRESS_VERSION', "3.0.0");
     /**
      * @deprecated 2.2.0
      */
@@ -30,7 +30,7 @@ if ( ! defined('EMBEDPRESS_VERSION')) {
 }
 
 if ( ! defined('EMBEDPRESS_PRO_VERSION')) {
-    define('EMBEDPRESS_PRO_VERSION', "2.4.6");
+    define('EMBEDPRESS_PRO_VERSION', "3.0.0");
 }
 
 if ( ! defined('EMBEDPRESS_ROOT')) {
