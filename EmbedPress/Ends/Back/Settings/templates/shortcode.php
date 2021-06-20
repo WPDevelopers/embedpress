db0c3fe94e9cd08861b530969f3d0703df22a23a
Kamal Ahmed <kamalacca@gmail.com>
2021-06-20 13:44:13 +0600

    updated text in the shortcode page

--- EmbedPress/Ends/Back/Settings/templates/shortcode.php
+++ EmbedPress/Ends/Back/Settings/templates/shortcode.php
@@ -5,20 +5,20 @@
  */
 ?>
 <div class="embedpress__settings background__white radius-25 p40">
-	<h3>Shortcode</h3>
+	<h3><?php esc_html_e( "Shortcode", "embedpress" ); ?></h3>
 	<div class="embedpress__shortcode">
-		<p class="shortcode__text">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+		<p class="shortcode__text"><?php esc_html_e( "Insert any link in the following field and click on the 'Generate' button to get your shortcode. You can copy the code clicking on the 'Copy' button", "embedpress" ); ?></p>
 		<div class="shortcode__form form__inline mb30">
 			<div class="form__group">
-				<input type="url" id="ep-link" class="form__control" placeholder="Place your link here">
+				<input type="url" id="ep-link" class="form__control" placeholder="<?php esc_attr_e( "Place your link here to generate shortcode", "embedpress" ); ?>">
 			</div>
-			<button class="button button__themeColor radius-10" id="ep-shortcode-btn">Generate Shortcode</button>
+			<button class="button button__themeColor radius-10" id="ep-shortcode-btn"><?php esc_html_e( "Generate", "embedpress" ); ?></button>
 		</div>
 		<div class="shortcode__form form__inline">
 			<div class="form__group">
 				<input type="text" class="form__control" id="ep-shortcode" readonly>
 			</div>
-			<button class="button button__themeColor copy__button radius-10" id="ep-shortcode-cp"><i class="ep-icon ep-copy"></i></button>
+			<button class="button button__themeColor copy__button radius-10" id="ep-shortcode-cp"><i class="ep-icon ep-copy"></i><?php esc_html_e( "Copy", "embedpress" ); ?></button>
 		</div>
 	</div>
 </div>	
