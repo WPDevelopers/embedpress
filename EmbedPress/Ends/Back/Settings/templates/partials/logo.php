bb1fbe22c93409b64a86d66a180771f15d49048a
Md. Alimuzzaman Alim <alim@wpdeveloper.com>
2022-06-07 12:28:09 +0600

    Update logo.php

--- EmbedPress/Ends/Back/Settings/templates/partials/logo.php
+++ EmbedPress/Ends/Back/Settings/templates/partials/logo.php
@@ -6,9 +6,9 @@
 <header class="embedpress-header">
     <a href="#" class="site__logo"><img src="<?php echo EMBEDPRESS_SETTINGS_ASSETS_URL; ?>img/logo.svg" alt=""></a>
     <ul class="embedpress-version-wrapper">
-        <li>Core Version: V<?php echo EMBEDPRESS_VERSION; ?></li>
+        <li>Core Version: v<?php echo EMBEDPRESS_VERSION; ?></li>
 	    <?php if ( $pro_active ) { ?>
-            <li>Pro Version: V<?php echo get_embedpress_pro_version(); ?></li>
+            <li>Pro Version: v<?php echo get_embedpress_pro_version(); ?></li>
 	    <?php }?>
     </ul>
 </header>
