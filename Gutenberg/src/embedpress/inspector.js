fef66f48b97707be44f261117e5edf7ec643152a
Akash M <akash.m.diu@gmail.com>
2023-01-30 11:51:46 +0600

    fixed height/width control issue for gutenberg

--- Gutenberg/src/embedpress/inspector.js
+++ Gutenberg/src/embedpress/inspector.js
@@ -67,7 +67,7 @@ export default function Inspector({ attributes, setAttributes, isYTChannel, isYT
                                     }
 
                                     {
-                                        (videosize == 'responsive') && (
+                                        ((isYTVideo || isVimeoVideo) && (videosize == 'responsive'))  && (
                                             <p>{__("You can adjust the width of embedded content.")}</p>
                                         )
                                     }
@@ -88,7 +88,7 @@ export default function Inspector({ attributes, setAttributes, isYTChannel, isYT
                                     />
 
                                     {
-                                        ((!isYTVideo || !isVimeoVideo) && (videosize == 'fixed')) && (
+                                        ((!isYTVideo && !isVimeoVideo) || (videosize == 'fixed'))  && (
                                             <TextControl
                                                 label={__("Height")}
                                                 value={height}
