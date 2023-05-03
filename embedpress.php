92a292333d4b9a69f3591f501f9a599f8115a699
Akash M <akash.m.diu@gmail.com>
2023-05-03 14:22:18 +0600

    Update embedpress.php

--- embedpress.php
+++ embedpress.php
@@ -122,7 +122,7 @@ function get_embed_type() {
 }
 
 
-function track_embed_usage() {
+function ep_track_embed_usage() {
     // Get the type of embed used (e.g. "youtube", "vimeo", "google_doc", etc.)
     $embed_type = 'youtube';
 
@@ -130,11 +130,11 @@ function track_embed_usage() {
     global $wpdb;
     $wpdb->insert( 'embed_usage_log', array( 'embed_type' => $embed_type, 'user_id' => get_current_user_id() ) );
 }
-add_action( 'embed_content', 'track_embed_usage' );
+add_action( 'embed_content', 'ep_track_embed_usage' );
 
 
-function enqueue_jquery_in_header() {
+function ep_enqueue_jquery_in_header() {
     wp_enqueue_script( 'jquery', false, array(), false, true );
 }
-add_action( 'wp_enqueue_scripts', 'enqueue_jquery_in_header', 1 );
+add_action( 'wp_enqueue_scripts', 'ep_enqueue_jquery_in_header', 1 );
 
