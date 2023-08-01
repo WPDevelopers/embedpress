eaab611fbe937241f58531af3a2f392b9fdd84cd
Akash M <akash.m.diu@gmail.com>
2023-08-01 15:49:22 +0600

    added gumroud
    Added gumroud src provider and support all public page embed

--- providers.php
+++ providers.php
@@ -15,6 +15,8 @@
  */
 
  $host_url = parse_url(site_url());
+ 
+
 $additionalServiceProviders = [
 	EMBEDPRESS_NAMESPACE . "\\Providers\\GoogleMaps" => ["google.com", "google.com.*", "maps.google.com", "goo.gl", "google.co.*"],
 	EMBEDPRESS_NAMESPACE . "\\Providers\\GoogleDrive" => ["drive.google.com"],
@@ -26,6 +28,6 @@ $additionalServiceProviders = [
 	EMBEDPRESS_NAMESPACE . "\\Providers\\OpenSea"    => ["opensea.io"],
 	EMBEDPRESS_NAMESPACE . "\\Providers\\NRKRadio"    => ["radio.nrk.no", "nrk.no"],
 	EMBEDPRESS_NAMESPACE . "\\Providers\\GitHub"    => ["gist.github.com", "github.com"],
-	EMBEDPRESS_NAMESPACE . "\\Providers\\SelfHosted"    => [$host_url['host']], // This is provider also uses for Wrapper
-
+	EMBEDPRESS_NAMESPACE . "\\Providers\\SelfHosted"    => [$host_url['host'], "*.com", "*.net", "*.org", "*.edu", "*.gov", "*.mil", "*.int", "*.biz", "*.info", "*.jobs", "*.mobi", "*.name", "*.aero", "*.coop", "*.travel", "*.museum", "*.cat", "*.pro", "*.tel", "*.asia", "*.xxx", "*.post", "*.geo", "*.nato"], 
+	EMBEDPRESS_NAMESPACE . "\\Providers\\Gumroad"    => ["*.gumroad.com","gumroad.com"], 
 ];
