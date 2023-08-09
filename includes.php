bc13877ab3e1b1222df320512f11223d38c2e94c
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2023-08-09 13:49:29 +0600

    update changelog

--- includes.php
+++ includes.php
@@ -22,7 +22,7 @@ if ( ! defined('EMBEDPRESS_PLG_NAME')) {
 }
 
 if ( ! defined('EMBEDPRESS_VERSION')) {
-	define('EMBEDPRESS_VERSION', "3.8.2");
+	define('EMBEDPRESS_VERSION', "3.8.3");
 	/**
 	 * @deprecated 2.2.0
 	 */
