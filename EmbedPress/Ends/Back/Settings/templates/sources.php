431c6fb795d1529a463294dbbbdc795d8afae476
Akash M <akash.m.diu@gmail.com>
2023-09-19 13:35:44 +0600

    added source template

---
+++ EmbedPress/Ends/Back/Settings/templates/sources.php
@@ -0,0 +1,10 @@
+<div class="source-settings-page">
+    <div class="tab-button-section">
+        <ul class="source-tab">
+            <li>This is a conten</li>
+        </ul>
+    </div>
+    <div class="tab-content-section">
+
+    </div>
+</div>
