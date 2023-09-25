bfe9ba56b59085e6a68798cc6b3fa0f446d07a1a
Mozammel Ali <sapanmozammel@gmail.com>
2023-09-25 16:30:39 +0600

    shortcode tab

--- EmbedPress/Ends/Back/Settings/templates/shortcode.php
+++ EmbedPress/Ends/Back/Settings/templates/shortcode.php
@@ -8,7 +8,7 @@
 	<h3><?php esc_html_e( "Shortcode", "embedpress" ); ?></h3>
 	<div class="embedpress__shortcode">
 		<p class="shortcode__text"><?php printf( esc_html__( "EmbedPress has direct integration with Classic, Gutenberg and Elementor Editor. But for other page editor you can use EmbedPress shortcode feature. To generate shortcode simply insert your link, click %s'Generate'%s button and then copy your shortcode. For details, check out this %sdocumentation%s.", "embedpress" ),'<strong>', '</strong>','<a class="ep-link" href="https://embedpress.com/docs/how-to-use-embedpress-shortcodes-page-builders/" target="_blank">', '</a>'); ?></p>
-		<div class="shortcode__form form__inline mb30">
+		<div class="shortcode__form form__inline mb-20">
 			<div class="form__group">
 				<input type="url" id="ep-link" class="form__control" placeholder="<?php esc_attr_e( "Place your link here to generate shortcode", "embedpress" ); ?>">
 			</div>
@@ -18,7 +18,7 @@
 			<div class="form__group">
 				<input type="text" class="form__control" id="ep-shortcode" readonly>
 			</div>
-			<button class="button button__themeColor copy__button radius-10" id="ep-shortcode-cp"><i class="ep-icon ep-copy"></i></button>
+			<button class="button button__themeColor copy__button radius-10" id="ep-shortcode-cp"><i class="ep-icon ep-copy"></i><span>Copy Link</span></button>
 		</div>
 	</div>
 </div>	
