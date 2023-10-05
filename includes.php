1327c61c0683d73e45040efe80887ac1a1256ed8
Nahid Hasan Tushar <tusharhasan08@gmail.com>
2023-10-05 13:46:39 +0600

    Readme Updated v3.9.0

--- includes.php
+++ includes.php
@@ -22,7 +22,7 @@ if ( ! defined('EMBEDPRESS_PLG_NAME')) {
 }
 
 if ( ! defined('EMBEDPRESS_VERSION')) {
-	define('EMBEDPRESS_VERSION', "3.8.5");
+	define('EMBEDPRESS_VERSION', "3.9.0");
 	/**
 	 * @deprecated 2.2.0
 	 */
