8a1adbe3478bd2df330013dbf5ef339f10d2cca1
Nahid Hasan Tushar <tusharhasan08@gmail.com>
2024-05-21 15:04:10 +0600

    version updated 4.0.0

--- includes.php
+++ includes.php
@@ -22,7 +22,7 @@ if ( ! defined('EMBEDPRESS_PLG_NAME')) {
 }
 
 if ( ! defined('EMBEDPRESS_VERSION')) {
-	define('EMBEDPRESS_VERSION', "3.9.17");
+	define('EMBEDPRESS_VERSION', "4.0.0");
 	/**
 	 * @deprecated 2.2.0
 	 */
