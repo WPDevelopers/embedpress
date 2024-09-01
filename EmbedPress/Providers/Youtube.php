8b03bf258c1cc3a1cd1573811de7f32baeab6872
Akash <akash.m.diu@gmail.com>
2024-09-01 09:59:54 +0600

    Update Youtube.php

--- EmbedPress/Providers/Youtube.php
+++ EmbedPress/Providers/Youtube.php
@@ -404,7 +404,7 @@ class Youtube extends ProviderAdapter implements ProviderInterface {
                 $main_iframe = "<div class='ep-first-video'><iframe width='{$params['maxwidth']}' height='{$params['maxheight']}' src='$rel' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen title='{$title}'></iframe></div>";
             }
 
-            if (!is_embedpress_pro_active() && isset($params['ytChannelLayout']) && ($params['ytChannelLayout'] == 'grid' || $params['ytChannelLayout'] == 'carousel')) {
+            if (!apply_filters('embedpress/is_allow_rander', false) && isset($params['ytChannelLayout']) && ($params['ytChannelLayout'] == 'grid' || $params['ytChannelLayout'] == 'carousel')) {
                 return [];
             }            
 
@@ -631,13 +631,17 @@ class Youtube extends ProviderAdapter implements ProviderInterface {
                                     echo YoutubeLayout::create_gallery_layout($jsonResult, $gallobj, $options, $data, $channelTitle, $channelThumb); 
                                 }
                                 else if($layout === 'grid'){
-                                    echo YoutubeLayout::create_grid_layout($jsonResult, $gallobj, $options, $data, $channelTitle, $channelThumb); 
+                                    // echo YoutubeLayout::create_grid_layout($jsonResult, $gallobj, $options, $data, $channelTitle, $channelThumb); 
+
+                                    do_action('embedpress/youtube_grid_layout', $jsonResult, $gallobj, $options, $data, $channelTitle, $channelThumb);
                                 }
                                 else if($layout === 'list'){
                                     echo YoutubeLayout::create_list_layout($jsonResult, $gallobj, $options, $data, $channelTitle, $channelThumb); 
                                 }
                                 else if($layout === 'carousel'){
-                                    echo YoutubeLayout::create_carousel_layout($jsonResult, $gallobj, $options, $data, $channelTitle, $channelThumb); 
+                                    // echo YoutubeLayout::create_carousel_layout($jsonResult, $gallobj, $options, $data, $channelTitle, $channelThumb); 
+                                    do_action('embedpress/youtube_carousel_layout', $jsonResult, $gallobj, $options, $data, $channelTitle, $channelThumb);
+
                                 }
                                 else{
                                     echo YoutubeLayout::create_grid_layout($jsonResult, $gallobj, $options, $data, $channelTitle, $channelThumb); 
