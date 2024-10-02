ce3bf1f1f764b117915b1be1288e7633bf27c851
Akash <akash.m.diu@gmail.com>
2024-10-02 10:59:58 +0600

    fixed qa issues

--- EmbedPress/Ends/Back/Settings/templates/main-template.php
+++ EmbedPress/Ends/Back/Settings/templates/main-template.php
@@ -29,13 +29,11 @@
             <div class="intro-text_wrapper">
                 <h4 class="intro-header">Get Started with EmbedPress</h4>
                 <p class="intro-sub_header">The premium version helps us to continue development of the product incorporating even more features and enhancements. You will also get world class support from our dedicated team, 24/7.</p>
-                <a href="https://embedpress.com/documentation/" class="intro-docu_btn">Documentation</a>
+                <a href="https://embedpress.com/documentation/" target="_blank" class="intro-docu_btn">Documentation</a>
             </div>
             <div class="popup-video-wrap"></div>
 
         </div>
-
-
 
 
         <div class="embedpress-body mb30">
