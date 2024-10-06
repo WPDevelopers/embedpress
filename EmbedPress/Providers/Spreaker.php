d5bef9f42e33b0c23920cbb0af95b23889d595da
Akash <akash.m.diu@gmail.com>
2024-10-06 17:29:01 +0600

    added spreaker

---
+++ EmbedPress/Providers/Spreaker.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace EmbedPress\Providers;
+
+use Embera\Provider\ProviderAdapter;
+use Embera\Provider\ProviderInterface;
+use Embera\Url;
+
+(defined('ABSPATH') && defined('EMBEDPRESS_IS_LOADED')) or die("No direct script access allowed.");
+
+/**
+ * Entity responsible to support Spreaker embeds.
+ *
+ * @package     EmbedPress
+ * @subpackage  EmbedPress/Providers
+ * @author      EmbedPress <help@embedpress.com>
+ * @copyright   Copyright (C) 2023 WPDeveloper. All rights reserved.
+ * @license     GPLv3 or later
+ * @since       1.0.0
+ */
+class Spreaker extends ProviderAdapter implements ProviderInterface
+{
+    /** inline {@inheritdoc} */
+    protected static $hosts = ["tiktok.com"];
+    /**
+     * Method that verifies if the embed URL belongs to Spreaker.
+     *
+     * @param Url $url
+     * @return  boolean
+     * @since   1.0.0
+     *
+     */
+    public function validateUrl(Url $url)
+    {
+        return (bool) (preg_match('~spreaker\.com/show/([^/]+)~i', (string) $url) ||
+            preg_match('~spreaker\.com/user/([^/]+)(/[^/]+)?~i', (string) $url));
+    }
+
+    public function validateSpreaker($url)
+    {
+        return (bool) (preg_match('~spreaker\.com/show/([^/]+)~i', (string) $url) ||
+            preg_match('~spreaker\.com/user/([^/]+)(/[^/]+)?~i', (string) $url));
+    }
+
+    public function extractPodcastId($url)
+    {
+        preg_match("/(\d+)$/", $url, $matches);
+
+        if (isset($matches[1])) {
+            return $matches[1]; // Return the ID
+        }
+
+        return null;
+    }
+
+
+
+    /**
+     * This method fakes an Oembed response.
+     *
+     * @since   1.0.0
+     *
+     * @return  array
+     */
+    public function fakeResponse()
+    {
+        $src_url = urldecode($this->url);
+
+
+        // Check if the url is already converted to the embed format  
+        if ($this->validateSpreaker($src_url)) {
+            $iframeSrc = 'https://widget.spreaker.com/player?show_id=' . $this->extractPodcastId($src_url) . '&theme=dark&playlist=show&playlist-continuous=true&chapters-image=true';
+        } else {
+            return [];
+        }
+
+
+        $width = isset($this->config['maxwidth']) ? $this->config['maxwidth'] : 600;
+        $height = isset($this->config['maxheight']) ? $this->config['maxheight'] : 350;
+
+        return [
+            'type'          => 'rich',
+            'provider_name' => 'Spreaker',
+            'provider_url'  => 'https://spreaker.com',
+            'title'         => 'Unknown title',
+            'html'          => '<iframe title=""  width="' . esc_attr($width) . '" height="' . esc_attr($height) . '" src="' . esc_url($iframeSrc) . '" ></iframe>',
+        ];
+    }
+    /** inline @inheritDoc */
+    public function modifyResponse(array $response = [])
+    {
+        return $this->fakeResponse();
+    }
+}
