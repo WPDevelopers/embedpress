7324982f25b2f69bd7c173e20388a06a8cf6631d
Akash <akash.m.diu@gmail.com>
2024-10-22 14:47:15 +0600

    added halloween notice

--- EmbedPress/Includes/Traits/Shared.php
+++ EmbedPress/Includes/Traits/Shared.php
@@ -161,21 +161,41 @@ trait Shared
         //     ]
         // );
 
-        $b_message            = '<p style="margin-top: 0; margin-bottom: 10px;"><strong>Join Us in Celebrating 100K+ Users!</strong> Enjoy up to 30% OFF for EmbedPress PRO & embed from 150+ sources</p><a class="button button-primary" href="https://wpdeveloper.com/upgrade/embedpress-bfcm" target="_blank">Upgrade to PRO</a> <button data-dismiss="true" class="dismiss-btn button button-link">I don’t want to save money</button>';
-        $_black_friday_notice = [
+        // $b_message            = '<p style="margin-top: 0; margin-bottom: 10px;"><strong>Akah  Join Us in Celebrating 100K+ Users!</strong> Enjoy up to 30% OFF for EmbedPress PRO & embed from 150+ sources</p><a class="button button-primary" href="https://wpdeveloper.com/upgrade/embedpress-bfcm" target="_blank">Upgrade to PRO</a> <button data-dismiss="true" class="dismiss-btn button button-link">I don’t want to save money</button>';
+        // $_black_friday_notice = [
+        //     'thumbnail' => $_assets_url . 'images/full-logo.svg',
+        //     'html'      => $b_message,
+        // ];
+
+        // $notices->add(
+        //     '100k_notice',
+        //     $_black_friday_notice,
+        //     [
+        //         'start'       => $notices->time(),
+        //         'recurrence'  => false,
+        //         'dismissible' => true,
+        //         'refresh'     => EMBEDPRESS_VERSION,
+        //         "expire"      => strtotime('11:59:59pm 12th September, 2024'),
+        //         'display_if'  => !is_plugin_active('embedpress-pro/embedpress-pro.php')
+        //     ]
+        // );
+
+        $b_message            = '<div class="helloween_2024_notice"><p style="margin-top: 0; margin-bottom: 0px;">🎃 Unlock advanced embedding functionalities with EmbedPress PRO & enjoy <strong>Up to $150 Off</strong> this Halloween.</p><a class="button button-primary" href="https://wpdeveloper.com/upgrade/embedpress-bfcm" target="_blank">
+        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="m15.743 10.938.161-1.578c.086-.842.142-1.399.098-1.749h.015c.727 0 1.316-.622 1.316-1.389s-.589-1.389-1.315-1.389c-.727 0-1.316.622-1.316 1.39 0 .346.12.663.32.907-.287.186-.66.58-1.223 1.171-.434.456-.65.684-.893.72a.7.7 0 0 1-.394-.059c-.223-.104-.372-.385-.67-.95l-1.57-2.97a22 22 0 0 0-.476-.873c.569-.306.958-.93.958-1.65C10.754 1.496 9.97.667 9 .667s-1.754.829-1.754 1.852c0 .72.389 1.344.958 1.65-.139.234-.293.525-.476.873l-1.57 2.97c-.298.565-.447.846-.67.95a.7.7 0 0 1-.394.058c-.242-.035-.46-.263-.893-.719-.563-.592-.937-.985-1.223-1.171.2-.244.32-.56.32-.908 0-.767-.589-1.389-1.316-1.389-.726 0-1.315.622-1.315 1.39 0 .766.589 1.388 1.315 1.388h.016c-.045.35.012.906.098 1.749l.16 1.578c.09.876.164 1.71.255 2.46H15.49c.09-.75.165-1.584.254-2.46m-7.698 6.395h1.908c2.488 0 3.732 0 4.562-.784.362-.342.591-.959.757-1.762H2.727c.166.803.395 1.42.757 1.762.83.784 2.074.784 4.562.784" fill="#fff"/></svg> Upgrade to PRO</a></div>';
+        $_helloween_2024_notice = [
             'thumbnail' => $_assets_url . 'images/full-logo.svg',
             'html'      => $b_message,
         ];
 
         $notices->add(
-            '100k_notice',
-            $_black_friday_notice,
+            'helloween_2024_notice',
+            $_helloween_2024_notice,
             [
                 'start'       => $notices->time(),
                 'recurrence'  => false,
                 'dismissible' => true,
                 'refresh'     => EMBEDPRESS_VERSION,
-                "expire"      => strtotime('11:59:59pm 12th September, 2024'),
+                "expire"      => strtotime('11:59:59pm 3rd November, 2024'),
                 'display_if'  => !is_plugin_active('embedpress-pro/embedpress-pro.php')
             ]
         );
