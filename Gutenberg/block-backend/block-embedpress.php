d89b399ff9a15960f6c74348c00ebe45a13c54ba
Akash <akash.m.diu@gmail.com>
2025-04-08 15:50:01 +0600

    fixed: youtube responsive height issue

--- Gutenberg/block-backend/block-embedpress.php
+++ Gutenberg/block-backend/block-embedpress.php
@@ -507,8 +507,7 @@ function embedpress_render_block_style($attributes)
 
 		  .ose-youtube' . esc_attr($uniqid) . ' > iframe {
 			width: 100%;
-			height: 100%;
-			max-height:100%;
+			max-height:100%!important;
 		  }
 
 		  .ep-video-responsive{
