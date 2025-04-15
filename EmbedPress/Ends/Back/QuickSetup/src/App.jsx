2a674f962204768c83bd2b5cba6e39e3155b52bd
Akash <akash.m.diu@gmail.com>
2025-04-15 10:52:05 +0600

    added quick setup

---
+++ EmbedPress/Ends/Back/QuickSetup/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>I am doing some shitty things</>
+  )
+}
+
+export default App
