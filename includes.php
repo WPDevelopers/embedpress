16e157fd889b7db36d2d3b71bbf02dd3cb2769da
Akash <akash.m.diu@gmail.com>
2025-04-28 09:09:01 +0600

    version updated

--- includes.php
+++ includes.php
@@ -22,7 +22,7 @@ if ( ! defined('EMBEDPRESS_PLG_NAME')) {
 }
 
 if ( ! defined('EMBEDPRESS_VERSION')) {
-	define('EMBEDPRESS_VERSION', "4.2.4");
+	define('EMBEDPRESS_VERSION', "4.2.5");
 	/**
 	 * @deprecated 2.2.0
 	 */
