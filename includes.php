12de7266fd8602923308d04e3c6e193911f9e09d
Akash <akash.m.diu@gmail.com>
2025-07-14 12:14:49 +0600

    version bump

--- includes.php
+++ includes.php
@@ -22,7 +22,7 @@ if ( ! defined('EMBEDPRESS_PLG_NAME')) {
 }
 
 if ( ! defined('EMBEDPRESS_VERSION')) {
-	define('EMBEDPRESS_VERSION', "4.2.8");
+	define('EMBEDPRESS_VERSION', "4.2.9");
 	/**
 	 * @deprecated 2.2.0
 	 */
