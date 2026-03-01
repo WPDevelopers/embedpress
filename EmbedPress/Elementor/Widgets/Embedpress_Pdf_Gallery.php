50729516fb90b90f692a7fd0868cbc52f23f03b3
Akash <akash.m.diu@gmail.com>
2026-03-01 14:36:08 +0600

    feat: Introduce lightbox display mode with custom thumbnail functionality for PDF embeds.

--- EmbedPress/Elementor/Widgets/Embedpress_Pdf_Gallery.php
+++ EmbedPress/Elementor/Widgets/Embedpress_Pdf_Gallery.php
@@ -879,8 +879,8 @@ class Embedpress_Pdf_Gallery extends Widget_Base
                                 <path d="M15 3l2.3 2.3-2.89 2.87 1.42 1.42L18.7 6.7 21 9V3h-6zM3 9l2.3-2.3 2.87 2.89 1.42-1.42L6.7 5.3 9 3H3v6zm6 12l-2.3-2.3 2.89-2.87-1.42-1.42L5.3 17.3 3 15v6h6zm12-6l-2.3 2.3-2.87-2.89-1.42 1.42 2.89 2.87L15 21h6v-6z"/>
                             </svg>
                         </div>
-                        <div class="ep-pdf-gallery__book-title"><?php echo esc_html($pdf_name); ?></div>
                     </div>
+                    <div class="ep-pdf-gallery__book-title"><?php echo esc_html($pdf_name); ?></div>
                 </div>
                 <?php endforeach; ?>
 
