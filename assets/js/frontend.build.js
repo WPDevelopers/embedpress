748c72ebdb57b214224d0587e2dd089c92357d9a
Akash <akash.m.diu@gmail.com>
2026-04-12 14:55:56 +0600

    build

--- assets/js/frontend.build.js
+++ assets/js/frontend.build.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-  initializeEmbeds();
-});
-function initializeEmbeds() {
-  const embeds = document.querySelectorAll(".embedpress-block, .embedpress-embed");
-  embeds.forEach((embed) => {
-  });
-}
-//# sourceMappingURL=frontend.build.js.map
+document.addEventListener("DOMContentLoaded",function(){e()});function e(){document.querySelectorAll(".embedpress-block, .embedpress-embed").forEach(n=>{})}
